341aa72548f628b71d5037f7e3c26a815d11a1e7
Heijden <Kiki.vanderHeijden@unimaas.nl>
2020-12-21 11:58:48 +0100

    2020_12_21

---
+++ LoadAndProcess_Sounds_Evaluation.py
@@ -0,0 +1,91 @@
+# script to check files in folder
+
+# set directories
+dir_anfiles = "/workspace/notebooks/sounds_small/eval" # for DSRI
+dir_wrfiles = "/workspace/notebooks/sounds_small_npy/eval"
+
+# import necessary packages and libraries
+import os # to get info about directories
+import numpy as np
+import math
+from pytictoc import TicToc 
+t = TicToc() # create instant of class
+from scipy.io import loadmat
+
+# set parameters
+time_sound = 750 # time dimension of sound files, i.e. number of samples
+nfreqs     = 99 # nr of freqs used
+
+# start script
+t.tic()
+countfiles = 0
+with os.scandir(dir_anfiles) as listfiles:
+    for entry in listfiles:
+        countfiles = countfiles+1
+        #print(entry.name) # use this to display all file names
+        #print(temploc) 
+        #print(trainlabels)  
+t.toc("scanning files took")
+print("this folder contains" , countfiles, "files" )
+
+t.tic()
+# create array of location labels for the x and y coordinates. labels range from -1 to 1 in correspondence with the unit circle 
+# take labels from directory for left channel but they are the same for the left and right channel
+evallabels_x = [] # initialize list of labels
+evallabels_y = []
+filenames = [] # initialize list of original file names
+with os.scandir(dir_anfiles) as listfiles:
+    for entry in listfiles:
+        # derive location from filename 
+        filenames.append(entry.name[:-4]) # do not include extension
+        temploc = int(entry.name[1:4]) # get azimuth location and convert to integer
+        # note that the naming of the files has 0 at front, while for the unit circle 0 should be at the right, correct this first
+        if temploc >= 0 and temploc <= 90:
+            temploc = np.abs(temploc - 90)
+        elif temploc > 90:
+            temploc = np.abs(temploc - 90- 360)
+        temp_xcoord = np.around(math.cos(math.radians(temploc)),3,out=None) # math.cos operates on radians so convert angle to rad first
+        temp_ycoord = np.around(math.sin(math.radians(temploc)),3,out=None)
+        evallabels_x.append(temp_xcoord)
+        evallabels_y.append(temp_ycoord) 
+# add together in 2D array where column 1 = x coord and column 2 = y coord
+evallabels = np.vstack((np.array(evallabels_x),np.array(evallabels_y)))
+evallabels = np.transpose(evallabels)
+ 
+t.toc("creating the labels took")
+print("shape of labels is ", evallabels.shape)    
+
+t.tic()
+fileidx = 0
+countfilesdone = 0
+countfiles100 = 0
+# load files as list and convert to numpy array later to save time
+eval_an_l = [None]*evallabels.shape[0] #intialize list
+eval_an_r = [None]*evallabels.shape[0]
+with os.scandir(dir_anfiles) as listfiles:
+    for entry in listfiles:
+        eval_an_l[fileidx] = loadmat(dir_anfiles+"/"+entry.name,verify_compressed_data_integrity=False)['AN_l'] # note that indexing is faster than appending
+        eval_an_r[fileidx] = loadmat(dir_anfiles+"/"+entry.name,verify_compressed_data_integrity=False)['AN_r']
+        countfilesdone = countfilesdone+1
+        fileidx = fileidx+1
+        # this is just for  debugging to keep track of the time/speed
+        if countfilesdone == 100:
+            print(countfilesdone*countfiles100, " files done")
+            countfiles100 = countfiles100+1
+            t.toc("these 100 files took")
+            t.toc(restart=True)
+            countfilesdone = 0
+t.toc("loading the sounds took ")
+print("shape of sounds is", len(eval_an_l))
+# convert to nunmpy array
+eval_an_l_array = np.asarray(eval_an_l)
+eval_an_r_array = np.asarray(eval_an_r)
+print("shape of the sound array is ", eval_an_l_array.shape)
+
+# save numpy arrays and file names
+np.save(dir_wrfiles+"/an_l_sounds.npy",eval_an_l_array)
+np.save(dir_wrfiles+"/an_r_sounds.npy",eval_an_r_array)
+np.save(dir_wrfiles+"/labels_sounds.npy",evallabels)
+
+import pickle
+pickle.dump(filenames, open(dir_wrfiles+'/listfilenames_sounds.p','wb'))
---
+++ PrepareData_Evaluation.py
@@ -0,0 +1,84 @@
+
+# read and preprocess sounds in the DSRI environment
+
+# define directories, add r before the name because a normal string cannot be used as a path, alternatives are 
+# using / or \\ instead of \
+dir_anfiles = "/workspace/notebooks/sounds_small_npy/eval" # directory for both channels
+#dir_anfiles = "C:/Users/kiki.vanderheijden/Documents/PYTHON/DataFiles" # sounds left channel
+
+# import packages and libraries
+import numpy as np
+#from sklearn.utils import shuffle
+from pytictoc import TicToc 
+t = TicToc() # create instant of class
+import pickle
+import math
+
+# set parameters
+testset = 0.25 # size of testset
+nrazlocs = 36 # number of azimuth locations
+azimuthrange = np.arange(0,360,10)
+
+t.tic()
+# =============================================================================
+# THESE ARE THE REAL ONES
+# load numpy arrays from disk
+an_l = np.load(dir_anfiles+"/an_l_sounds.npy")
+an_r = np.load(dir_anfiles+"/an_r_sounds.npy")
+labels = np.load(dir_anfiles+"/labels_sounds.npy")
+filenames = pickle.load(open(dir_anfiles+'/listfilenames_sounds.p','rb'))
+t.toc("loading the numpy arrays took ")
+# =============================================================================
+
+# retrieve labels from names
+names_val_angle = np.empty([len(filenames)])
+cnt1 = 0
+for x in filenames:
+    names_val_angle[cnt1] =int(x[1:4])
+    cnt1 += 1
+
+#cycle through the labels to get the indices of the angles
+indices_val_angle = np.empty([nrazlocs,int(len(filenames)/nrazlocs)])
+for x in range(nrazlocs):
+    indices_val_angle[x,] = np.where(names_val_angle == azimuthrange[x])[0]
+
+# compute overall number of sounds per location
+nrsounds_loc = math.floor(len(labels[:,0])/nrazlocs)
+
+# intiate empty matrics (or zero matrices)
+an_l_eval = np.zeros((nrazlocs*math.floor((1-testset)*nrsounds_loc),len(an_l[1,:,:]),len(an_l[1,1,:]))) # round down here because train_test_split rounds the train size down
+an_r_eval = np.zeros((nrazlocs*math.floor((1-testset)*nrsounds_loc),len(an_r[1,:,:]),len(an_r[1,1,:])))
+labels_eval = np.zeros((nrazlocs*math.floor((1-testset)*nrsounds_loc),len(labels[1,:])))
+filenames_eval = list()
+
+# first take a part away for evaluation
+for x in range(nrazlocs) :
+    #  now take all those sounds --> you don't need to shuffle them because os.scandir already read them in an arbitrary order
+    temp_idx = indices_val_angle[x,]
+    temp_idx = temp_idx.astype(int)
+    an_l_temp = an_l[temp_idx,]
+    an_r_temp = an_r[temp_idx,]
+    labels_temp = labels[temp_idx,]
+    filenames_temp = [filenames[i] for i in temp_idx] # indexing from a list is a bit cumbersome...
+    # add to the matrices in the correct positions
+    an_l_eval[x*len(an_l_temp):(x+1)*len(an_l_temp)] = an_l_temp
+    an_r_eval[x*len(an_r_temp):(x+1)*len(an_r_temp)] = an_r_temp
+    labels_eval[x*len(labels_temp):(x+1)*len(labels_temp)] = labels_temp
+    filenames_eval.extend(filenames_temp)
+
+# add a fourth dimension ('channel') to train_an_l and train_an_r which should be 1, this is needed for the input to the DNN
+an_l_eval = np.expand_dims(an_l_eval,axis = 3)
+an_r_eval = np.expand_dims(an_r_eval,axis = 3)
+
+#save numpy arrays for model evaluation after training
+np.save(dir_anfiles+"/an_l_eval_sounds.npy",an_l_eval)
+np.save(dir_anfiles+"/an_r_eval_sounds.npy",an_r_eval)
+np.save(dir_anfiles+"/labels_eval_sounds.npy",labels_eval)
+pickle.dump(filenames_eval, open(dir_anfiles+'/listfilenames_evall_sounds.p','wb'))
+
+print("numpy arrays are saved to disk")
+print("Shape of evaluation sounds is:", an_l_eval.shape)
+print("Shape of evaluation labels is:", labels_eval.shape)
+
+
+        
